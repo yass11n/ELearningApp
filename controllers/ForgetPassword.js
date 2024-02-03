b1da7257d4ef92b3c2f1c4c27466fedf7e354058
yass11n <yassinyahia44@gmail.com>
2024-02-03 19:23:11 +0200

    final

--- controllers/ForgetPassword.js
+++
@@ -1,9 +0,0 @@
-const asyncHandler=require('express-async-handler');
-const User=require('../model/User');
-
-exports.forgetPassword = asyncHandler(async (req,res) =>{
-    const user= await User.findOne({email : req.body.email});
-    if(!user){
-        res.status(404).json({msg : `No user for this email ${req.body.email}`});
-    }
-});
