ac6ebef31a95922bd92ec715704d8d2aceb555aa
yass11n <yassinyahia44@gmail.com>
2024-04-01 14:52:30 +0200

    Module | Duration

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,12 @@ const { Router } = require("express");
 
 const routes = Router();
 
-routes.use("/api/v1/users" , require("./user.route"));
-routes.use("/api/v1/auth" , require("./auth.route"));
-routes.use("/api/v1/category" , require("./category.route"));
-routes.use("/api/v1/course" , require("./course.route"));
-routes.use("/api/v1/coursemodule" , require("./module.route"));
-routes.use("/api/v1/section" ,require("./section.route"));
-routes.use("/api/v1/cart" ,require("./cart.route"));
-routes.use("/api/v1/coupon" ,require("./coupon.route"));
-
-
+routes.use("/api/v1/users", require("./user.route"));
+routes.use("/api/v1/auth", require("./auth.route"));
+routes.use("/api/v1/category", require("./category.route"));
+routes.use("/api/v1/course", require("./course.route"));
+routes.use("/api/v1/coursemodule", require("./module.route"));
+routes.use("/api/v1/section", require("./section.route"));
+routes.use("/api/v1/cart", require("./cart.route"));
+routes.use("/api/v1/coupons", require("./coupon.route"));
 module.exports = routes;
